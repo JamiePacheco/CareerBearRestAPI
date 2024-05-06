b97f823d36303e2b780c7e88edd3a0dde11b0af4
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2024-05-06 02:23:30 -0400

    added new sub tables for bear interaction and user response

--- src/main/java/com/careerrbear/CareerBear/quiz/model/interactions/BearInteraction.java
+++ src/main/java/com/careerrbear/CareerBear/quiz/model/interactions/BearInteraction.java
@@ -16,13 +16,16 @@ public class BearInteraction implements Serializable {
     @Column(name = "BEAR_INTERACTION_ID")
     private Long id;
 
-    @Column(name = "CAREER_BEAR_PROMPT", length = 500)
-    private String careerBearPrompt;
+    @OneToOne
+    @Column(name = "CAREER_BEAR_PROMPT")
+    @JoinColumn(name = "CAREER_BEAR_ PROMPT_ID", referencedColumnName = "CAREER_BEAR_ PROMPT_ID")
+    private CareerBearPrompt careerBearPrompt;
 
+    @OneToOne
     @Column(name = "USER_RESPONSE")
-    private String userResponse;
+    @JoinColumn(name = "USER_RESPONSE_ID", referencedColumnName = "USER_RESPONSE_ID")
+    private UserResponse userResponse;
 
-//    @ManyToOne(cascade = CascadeType.ALL)
-//    @JoinColumn(name = "DETAILED_QUIZ_ID", nullable = false)
-//    private DetailedQuiz detailedQuiz;
+    @Column(name = "POSITION")
+    private int position;
 }
---
+++ src/main/java/com/careerrbear/CareerBear/quiz/model/interactions/UserResponse.java
@@ -0,0 +1,18 @@
+package com.careerrbear.CareerBear.quiz.model.interactions;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "USER_RESPONSE")
+public class UserResponse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_RESPONSE_ID")
+    private Long userResponseId;
+
+    @Column(name = "RESPONSE")
+    private String response;
+}
