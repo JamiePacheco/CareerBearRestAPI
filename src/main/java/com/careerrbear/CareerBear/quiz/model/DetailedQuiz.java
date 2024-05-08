fa3221d185bd23f1fa72bc0559d0389f75d84c20
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2024-05-08 18:07:01 -0400

    added json specification to detailedQuiz

--- src/main/java/com/careerrbear/CareerBear/quiz/model/DetailedQuiz.java
+++ src/main/java/com/careerrbear/CareerBear/quiz/model/DetailedQuiz.java
@@ -2,6 +2,8 @@ package com.careerrbear.CareerBear.quiz.model;
 
 import com.careerrbear.CareerBear.quiz.model.interactions.BearInteraction;
 import com.careerrbear.CareerBear.user.model.UserAccount;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -17,19 +19,23 @@ public class DetailedQuiz {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "DETAILED_QUIZ_ID")
+    @JsonProperty(namespace = "id")
     private Long detailedQuizId;
 
     @Column(name = "DATE_TAKEN")
     private LocalDate dateTaken;
 
     @OneToMany(cascade = CascadeType.ALL)
+    @JsonProperty(namespace = "interactions")
     private Set<BearInteraction> bearInteractions;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "QUIZ_RESULTS_ID", referencedColumnName = "QUIZ_RESULTS_ID")
+    @JsonProperty(namespace = "results")
     private QuizResults quizResults;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "USER_ACCOUNT_ID")
+    @JsonProperty(namespace = "userId")
     private UserAccount userAccount;
 }
